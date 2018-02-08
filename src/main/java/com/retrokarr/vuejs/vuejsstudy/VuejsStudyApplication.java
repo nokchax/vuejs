62ad42d1cc24a64f4fcbb4fafb05d8289b19915a
rogue <retrokarr@gmail.com>
2018-02-08 21:10:41 +0900

    init project

---
+++ src/main/java/com/retrokarr/vuejs/vuejsstudy/VuejsStudyApplication.java
@@ -0,0 +1,12 @@
+package com.retrokarr.vuejs.vuejsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuejsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuejsStudyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/retrokarr/vuejs/vuejsstudy/VuejsStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.retrokarr.vuejs.vuejsstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VuejsStudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
