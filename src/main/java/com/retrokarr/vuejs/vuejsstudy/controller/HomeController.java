4e438841e36830efcfe4b157da3bb9d79c5a82eb
rogue <retrokarr@gmail.com>
2018-02-10 21:43:37 +0900

    refactor : modify controller to receive several url

--- src/main/java/com/retrokarr/vuejs/vuejsstudy/controller/HomeController.java
+++ src/main/java/com/retrokarr/vuejs/vuejsstudy/controller/HomeController.java
@@ -1,15 +1,21 @@
 package com.retrokarr.vuejs.vuejsstudy.controller;
 
+import com.sun.istack.internal.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
+
 @Controller
-@RequestMapping("/{no}")
+@RequestMapping({"/{no}", ""})
 public class HomeController {
     @GetMapping
-    public String home(@PathVariable String no) {
-        return "vue" + no;
+    public String home(@PathVariable(required = false) String no) {
+        return Optional.ofNullable(no)
+                .map(n -> "vue" + n)
+                .orElse("index");
     }
 }
