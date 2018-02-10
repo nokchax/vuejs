12267cf55183e3b4edb33cef4c3044427ad2c997
rogue <retrokarr@gmail.com>
2018-02-10 21:36:55 +0900

    feat : add view resolver

---
+++ src/main/java/com/retrokarr/vuejs/vuejsstudy/config/WebConfiguration.java
@@ -0,0 +1,27 @@
+package com.retrokarr.vuejs.vuejsstudy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".html");
+
+        return viewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
--- src/main/java/com/retrokarr/vuejs/vuejsstudy/controller/HomeController.java
+++ src/main/java/com/retrokarr/vuejs/vuejsstudy/controller/HomeController.java
@@ -2,13 +2,14 @@ package com.retrokarr.vuejs.vuejsstudy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/{no}")
 public class HomeController {
     @GetMapping
-    public String home() {
-        return "index.html";
+    public String home(@PathVariable String no) {
+        return "vue" + no;
     }
 }
